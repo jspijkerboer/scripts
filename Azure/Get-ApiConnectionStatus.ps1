1847a670f071826aa7ada80c0382b1a555be5f0c
Jeff Spijkerboer <jeff.spijkerboer@avanade.com>
2022-10-05 10:40:32 +0200

    Sorted

---
+++ Azure/Get-ApiConnectionStatus.ps1
@@ -0,0 +1,12 @@
+﻿$resourcegroup = "DEVRG"
+$resources = Get-AzResource -ResourceGroupName $resourcegroup -ResourceType 'Microsoft.Web/connections' -Name '*_azureblob'
+foreach ($r in $resources) {
+    $resource = Get-AzResource -ResourceId $r.ResourceId
+    $statuses = $resource.Properties.statuses
+    if ($statuses) {
+        $statusError = $statuses[0].error
+        if ($statusError -and $statusError.code -eq "Unauthenticated") {
+            Write-Host $resource.Name is unauthenticated
+        }
+    }
+}
